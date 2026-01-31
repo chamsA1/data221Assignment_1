c3b15ce0f3618e95cc9410b4b90368cda0ed4b18
chamsA1 <deanchams2005@gmail.com>
2026-01-30 19:14:04 -0700

    Create DataFrame and add derived column

---
+++ Data221Assignment1Question8.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    "A": [1, 2, 2, 1],
+    "B": [3.1, 4.2, 1.5, 6.3],
+    "C": [800, 150, 400, 210],
+
+
+}
+dataFrame = pd.DataFrame(data)
+
+dataFrame["D"] = dataFrame["A"] * dataFrame["B"]
+
+print(dataFrame)
